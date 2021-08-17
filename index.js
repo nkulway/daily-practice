cdfcbfb7a1c0ab8c7b1618aae5deb521e2edfc19
Nick Kulway <nkulway@gmail.com>
2021-08-16 21:18:17 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+// manipulation of integers to strings and strings
+//     back to integers
+
+
+let thisnum = 9876;
+
+
+function squareDigits(num){
+let response = '';
+  let stringNum = JSON.stringify(num)
+  for (let i = 0; i < stringNum.length; i++) {
+  let squaredInt = parseInt(stringNum[i]) ** 2;
+    response += JSON.stringify(squaredInt);
+}
+  return response;
+}
+
+console.log(squareDigits(thisnum));
+
+
+// counting vowels in a string 
+//     count functionality 
+
+
+function vowel(word){
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let count = 0;
+      for (let i = 0; i < word.length; i++){
+      let letter = word[i];
+      if(vowels.includes(letter)) {
+        count += 1;
+        }
+    }
+    return count;
+    }
